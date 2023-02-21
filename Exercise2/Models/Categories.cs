69895441c42295dd3931ef6a29062becd618071c
CoasterCrook <grcrook03@icloud.com>
2023-02-21 17:01:13 -0500

    Added Models

---
+++ Exercise2/Models/Categories.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exercise2.Models
+{
+    public class Categories
+    {
+        [Key]
+        public string? CategoryId { get; set; }
+        public string? Name { get; set; }
+    }
+}
--- Exercise2/Models/Contacts.cs
+++ Exercise2/Models/Contacts.cs
@@ -1,6 +1,24 @@
-﻿namespace Exercise2.Models
+﻿using System.ComponentModel;
+
+namespace Exercise2.Models
 {
     public class Contacts
     {
+        public int Id { get; set; }
+        [DisplayName("First Name")]
+        public string? FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string? LastName { get; set; }
+        [DisplayName("Phone")]
+        public string? PhoneNumber { get; set; }
+        [DisplayName("Email")]
+        public string? Email { get; set; }
+        [DisplayName("Category")]
+        public int CategoriesId { get; set; }
+        public Categories? Categories { get; set; }
+        [DisplayName("Organization")]
+        public string? Organization { get; set; }
+        [DisplayName("Date Added")]
+        public DateTime Date { get; set; }
     }
 }
